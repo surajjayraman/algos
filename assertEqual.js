7407b768be688fc5a2f93e78dcda22a48afa397a
surajjayraman <suraj.jayraman@gmail.com>
2023-02-23 08:58:28 -0500

    Add console assert msgs.

--- assertEqual.js
+++ assertEqual.js
@@ -12,3 +12,13 @@ const assertEqual = function(actual, expected) {
 // TEST CODE
 assertEqual("Java", "JavaScript");
 assertEqual(1, 1);
+console.assert(1 === 1.1); // => prints out "Assertion failed" to the terminal
+
+// FUNCTION IMPLEMENTATION
+const sum = function(a, b) {
+  return a + b;
+};
+
+// TEST CODE
+console.assert(sum(1, 2) === 3);
+console.assert(sum(1, 20) === 3); // bad / incorrect assertion, and we see it fail!
