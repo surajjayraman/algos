e979e95521b5248b8a15244a664f2795a15bf660
surajjayraman <suraj.jayraman@gmail.com>
2023-02-25 11:16:38 -0500

    Reverse cmd line args and Print them out.

--- reverse.js
+++ reverse.js
@@ -1,5 +1,21 @@
 // JavaScript Demo: Array.reverse()
 
+// print process.argv
+const array = process.argv;
+
+let arrayArgs = [];
+
+for (let i = 0; i < (array.length - 2); i++) {
+  arrayArgs.push(array[i + 2]);
+}
+
+console.log("array arguments: ", arrayArgs);
+
+process.argv.forEach(function(val, index, array) {
+  console.log(index + ': ' + val);
+  
+});
+
 const array1 = ['one', 'two', 'three'];
 console.log('array1:', array1);
 // Expected output: "array1:" Array ["one", "two", "three"]
@@ -11,3 +27,9 @@ console.log('reversed:', reversed);
 // Careful: reverse is destructive -- it changes the original array.
 console.log('array1:', array1);
 // Expected output: "array1:" Array ["three", "two", "one"]
+
+// Print out reversed cmd line args:
+const reversedCmdLine = arrayArgs.reverse();
+console.log('reversed cmd line:', reversedCmdLine);
+
+
