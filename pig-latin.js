5894c19d31d58961c0cf4f000df01d8b3e990d61
surajjayraman <suraj.jayraman@gmail.com>
2023-03-05 10:36:57 -0500

    Add pig latin rules output script.

---
+++ pig-latin.js
@@ -0,0 +1,28 @@
+
+// print process.argv for pig latin program
+const array = process.argv;
+
+let arrayArgs = [];
+
+for (let i = 0; i < (array.length - 2); i++) {
+  arrayArgs.push(array[i + 2]);
+}
+
+const pigLatin = () => {
+  console.log("pig latin arguments: ", arrayArgs);
+  const reversedPigLatin = reverseString(arrayArgs);
+  return reversedPigLatin;
+};
+let finalReversed = [];
+// Print out entire arrays reversed in String format
+const reverseString = (arrayArgs) => {
+  for (const args of arrayArgs) {
+    const splitString = args.split("");
+    let splitStringHead = splitString.slice(1);
+    const pigLatinArray = splitStringHead.join('') + splitString[0] + 'ay';
+    finalReversed.push(pigLatinArray);
+  }
+  console.log(`pig latin output: ${finalReversed.join(' ')}`);
+  return finalReversed;
+};
+pigLatin();
