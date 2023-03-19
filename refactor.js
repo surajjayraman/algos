8a0f99c34c2a3e56813e7534555ae2b7a1f93db6
surajjayraman <suraj.jayraman@gmail.com>
2023-03-18 20:06:08 -0400

    Refactor if else conditional branch.

--- refactor.js
+++ refactor.js
@@ -1,15 +1,23 @@
 const loopyLighthouse = function(range, multiples, words) {
   for (let i = range[0]; i <= range[1]; i++) {
+
     if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       console.log(words[0] + words[1]);
-    } else if (i % multiples[1] === 0) {
-      console.log(words[1]);
-    } else if (i % multiples[0] === 0) {
-      console.log(words[0]);
-    } else {
-      console.log(i);
+      continue;
     }
+    
+    if (i % multiples[1] === 0) {
+      console.log(words[1]);
+      continue;
+    }
+    
+    if (i % multiples[0] === 0) {
+      console.log(words[0]);
+      continue;
+    }
+    
+    console.log(i);
   }
 };
 
-loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+loopyLighthouse([15,20], [2, 5], ["Batty", "Beacon"]);
