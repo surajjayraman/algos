eacf0456f8021b58248fee2df578ee5440ddcf6b
surajjayraman <suraj.jayraman@gmail.com>
2023-03-23 21:26:00 -0400

    Add template for masking arguments.

--- mask.js
+++ mask.js
@@ -1,1 +1,7 @@
 // take in a single string as a command line argument and print out an obfuscated version
+/*
+Every "a" in the string should be replaced with a "4".
+Every "e" in the string should be replaced with a "3".
+Every "o" (oh) in the string should be replaced with a "0" (zero).
+Every "l" (el) in the string should be replaced with a "1" (one).
+*/
