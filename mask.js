1c69a3c05f25bf2f9c93979d1430b90f425b16e3
surajjayraman <suraj.jayraman@gmail.com>
2023-03-23 21:29:11 -0400

    Add code to process cmd line args.

--- mask.js
+++ mask.js
@@ -5,3 +5,14 @@ Every "e" in the string should be replaced with a "3".
 Every "o" (oh) in the string should be replaced with a "0" (zero).
 Every "l" (el) in the string should be replaced with a "1" (one).
 */
+
+// print process.argv for mask program
+const array = process.argv;
+
+let arrayArgs = [];
+
+for (let i = 0; i < (array.length - 2); i++) {
+  arrayArgs.push(array[i + 2]);
+}
+
+console.log(arrayArgs);
