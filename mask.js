64ee2ed5ad60fd60486f578642d8f70c37a8c4e4
surajjayraman <suraj.jayraman@gmail.com>
2023-03-25 10:53:35 -0400

    Add function to mask a string.

--- mask.js
+++ mask.js
@@ -14,5 +14,25 @@ let arrayArgs = [];
 for (let i = 0; i < (array.length - 2); i++) {
   arrayArgs.push(array[i + 2]);
 }
+const splitString = arrayArgs[0].split('');
 
-console.log(arrayArgs);
+const maskString = (splitString) => {
+
+  for (let j = 0; j < splitString.length; j++) {
+    if (splitString[j] === 'a') {
+      splitString[j] = 4;
+    }
+    if (splitString[j] === 'e') {
+      splitString[j] = 3;
+    }
+    if (splitString[j] === 'o') {
+      splitString[j] = 0;
+    }
+    if (splitString[j] === 'l') {
+      splitString[j] = 1;
+    }
+  }
+  return (splitString.join(''));
+};
+
+console.log(maskString(splitString));
