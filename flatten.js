5deba764e5ba24ca94f7941dc63da435e2a99f4b
surajjayraman <suraj.jayraman@gmail.com>
2023-04-01 11:50:17 -0400

    Update flatArray func.

--- flatten.js
+++ flatten.js
@@ -8,24 +8,22 @@ const flatArray = (obj) => {
   let outputArray = [];
 
   if (Array.isArray(obj)) {
-
     for (let item of obj) {
       if (Array.isArray(item)) {
         for (let childItem of item) {
+          if (Array.isArray(childItem)) {
+            outputArray.push((flatArray(childItem)));
+            continue;
+          }
           outputArray.push(childItem);
         }
         continue;
       }
-
       outputArray.push(item);
-
     }
-
   }
-
   return outputArray;
-
 };
 
-console.log(flatArray([1, 2, [3, 4], 5, [6],[8,9,7]]));
-// Expected output: true
+console.log(flatArray([1, 2, [3, 4], 5, [6]]));
+console.log(flatArray([9,7,[1],[2,3]]));
