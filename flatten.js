b9c817ca2d73f68529e7d2f6588de4092dd3bfd0
surajjayraman <suraj.jayraman@gmail.com>
2023-04-01 22:16:31 -0400

    Update func to include a callback.

--- flatten.js
+++ flatten.js
@@ -3,14 +3,14 @@
 // and return a "flattened" version of the array.
 // flatten([1, 2, [3, 4], 5, [6]]) // => [1, 2, 3, 4, 5, 6]
 
-const flatArray = (obj) => {
+const flatArray = (obj, callback) => {
 
   let outputArray = [];
 
   if (Array.isArray(obj)) {
     for (let item of obj) {
       if (Array.isArray(item)) {
-        arrayFlatten(item, outputArray);
+        callback(item, outputArray);
         continue;
       }
       outputArray.push(item);
@@ -21,7 +21,6 @@ const flatArray = (obj) => {
 
 const arrayFlatten = (childObj, parentObj) => {
   if (Array.isArray(childObj)) {
-
     for (let childItem of childObj) {
       if (Array.isArray(childItem)) {
         arrayFlatten(childItem, parentObj);
@@ -29,11 +28,10 @@ const arrayFlatten = (childObj, parentObj) => {
       }
       parentObj.push(childItem);
     }
-
   }
   return;
 };
 
-console.log(flatArray([1, 2, [3, 4], 5, [6,[7,8,[9,[9]]]]]));
-console.log(flatArray([9,7,[1],[2,3,[9,[9,8,7]]],[2,[6,[9,8]]]]));
-console.log(flatArray([2, [3, 2]]));
+console.log(flatArray([1, 2, [3, 4], 5, [6,[7,8,[9,[9]]]]], arrayFlatten));
+console.log(flatArray([9,7,[1],[2,3,[9,[9,8,7]]],[2,[6,[9,8]]]], arrayFlatten));
+console.log(flatArray([2, [3, 2]], arrayFlatten));
