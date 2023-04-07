e556574c0e731e9463a63b71e4f008a46a136f6e
surajjayraman <suraj.jayraman@gmail.com>
2023-04-07 11:46:29 -0400

    Fix the isPalindrome func.

--- palindrome.js
+++ palindrome.js
@@ -1,17 +1,22 @@
 // FUNCTION IMPLEMENTATION (MULTIPLE BUGS)
 const isPalindrome = function(str) {
-  const noSpaces = str.split(" ").join("");
-  const midIndex = Math.floor(noSpaces.length / 2);
+  const noSpaces = str.toUpperCase().split(" ").join("").split('');
+  console.log(`No spaces ${noSpaces} type = ${typeof(noSpaces)}`);
   const lastIndex = noSpaces.length - 1;
+  console.log(`last index = ${lastIndex} and we will iterate until ${Math.round(lastIndex / 2)}`);
   
-  for (let i = 0; i < midIndex; i++) {
-    if (str[i] !== str[lastIndex - i]) return false;
+  for (let i = 0; i < Math.round(lastIndex / 2); i++) {
+    console.log(`noSpaces[i] = ${noSpaces[i]} and noSpaces[lastIndex - i] = ${noSpaces[lastIndex - i]}`);
+    if (noSpaces[i] !== noSpaces[lastIndex - i]) {
+      return false;
+    }
   }
+  return true;
 };
   
 // Assertion Function
 const assertPalindrome = function(word, expected) {
-  console.log(`Testing isPalindrome(\"${word}\"):`);
+  console.log(`Testing isPalindrome(${word}):`);
   const actual = isPalindrome(word);
   if (actual === expected) {
     console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ function returned ${actual}\n`);
@@ -22,15 +27,15 @@ const assertPalindrome = function(word, expected) {
   
   
 // TEST CODE
-// These should all pass assertion, but they don't.
+// These should all pass assertion
 assertPalindrome('p', true);
 assertPalindrome('racecar', true);
 assertPalindrome('my gym', true);
 assertPalindrome('foo', false);
-assertPalindrome('fluff', false);
-assertPalindrome('just some random words', false);
-assertPalindrome('malayalam', true);
+//assertPalindrome('fluff', false);
+//assertPalindrome('just some random words', false);
+//assertPalindrome('malayalam', true);
   
-// Bonus / Stretch: Uncomment these tests and figure out why these are also failing
-// assertPalindrome('Kayak', true);
-// assertPalindrome('a santa at NASA', true);
+// Bonus / Stretch: Uncomment these tests and make then work
+//assertPalindrome('Kayak', true);
+//assertPalindrome('a santa at NASA', true);
