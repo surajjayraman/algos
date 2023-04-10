712d714283dfa2e4dda460e3395e5855c9ca2ed2
surajjayraman <suraj.jayraman@gmail.com>
2023-04-10 12:32:04 -0400

    Fix bug in longestStr func.

--- printInFrame.js
+++ printInFrame.js
@@ -1,19 +1,22 @@
 const printInFrame = function(list) {
   list = list.split(' ');
+  console.log(`list = ${list}`);
   const longest = longestStr(list).length;
-  const border = repeat('*', longest);
+  console.log(`list[0]=${list[0]}, typeoflist = ${typeof(list)}`);
+  console.log(`longest = ${longest}`);
+  const border = repeat('*', (longest + 5));
   
   console.log(border);
   for (const word of list) {
-    console.log(`* ${word}${repeat(' ', longest - word.length + 1)}*`);
+    console.log(`* ${word}${repeat(' ', longest - word.length + 1)} *`);
   }
   console.log(border);
 };
   
 const repeat = function(str, times) {
-  let result = str;
+  let result = '';
   
-  for (let i = 0; i < times; i++) {
+  for (let i = 0; i < times-1; i++) {
     result += str;
   }
   
@@ -24,7 +27,10 @@ const longestStr = function(list) {
   let longest = list[0];
   
   for (const str of list) {
-    longest = str;
+    if (longest.length < str.length){
+        longest = str;
+    }
+    
   }
   
   return longest;
