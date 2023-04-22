8f631a258018cb98ed9a63ad34e7eeff9577ce85
surajjayraman <suraj.jayraman@gmail.com>
2023-04-22 00:03:44 -0400

    Add template JS file for sales tax.

--- sales.js
+++ sales.js
@@ -1,29 +1,29 @@
 const salesTaxRates = {
-    AB: 0.05,
-    BC: 0.12,
-    SK: 0.10
-  };
+  AB: 0.05,
+  BC: 0.12,
+  SK: 0.10
+};
   
-  const companySalesData = [
-    {
-      name: "Telus",
-      province: "BC",
-      sales: [ 100, 200, 400 ]
-    },
-    {
-      name: "Bombardier",
-      province: "AB",
-      sales: [ 80, 20, 10, 100, 90, 500 ]
-    },
-    {
-      name: "Telus",
-      province: "SK",
-      sales: [ 500, 100 ]
-    }
-  ];
-
-  const calculateSalesTax = function(salesData, taxRates) {
-    // Implement your code here
+const companySalesData = [
+  {
+    name: "Telus",
+    province: "BC",
+    sales: [ 100, 200, 400 ]
+  },
+  {
+    name: "Bombardier",
+    province: "AB",
+    sales: [ 80, 20, 10, 100, 90, 500 ]
+  },
+  {
+    name: "Telus",
+    province: "SK",
+    sales: [ 500, 100 ]
   }
+];
+
+const calculateSalesTax = function(salesData, taxRates) {
+  // Implement your code here
+};
 
   
