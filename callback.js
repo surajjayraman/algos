dd806df1b6cab512bae8a472c354a405c14c9522
surajjayraman <suraj.jayraman@gmail.com>
2023-05-13 11:20:35 -0400

    Update with forEach custom examples.

--- callback.js
+++ callback.js
@@ -12,3 +12,26 @@ myHigherOrderFunction(myFunction);
 const sayHello = name => `hello there ${name}`;
 const returnVal = sayHello('bob');
 console.log('returnVal:', returnVal);
+
+// single responsibility principle
+// loop through array
+const loopThroughArray = function(arr, callback) {
+  for (const element of arr) {
+    callback(element);
+  }
+};
+
+const giveATreat = function(petName) {
+  console.log(`Does ${petName} want some treats??????`);
+};
+
+const pets = ['Charlie!', 'Coda', 'Cadence', 'Molly', 'Joyce', 'Charlie', 'Dioji', 'Sweety'];
+
+loopThroughArray(pets, giveATreat);
+
+// use for each to implement ^
+pets.forEach(function(petName) {
+  console.log(`Does ${petName} want some treats? (using forEach)`);
+});
+
+pets.forEach(giveATreat);
