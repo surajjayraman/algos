1cb163ba9ecd2d9647b1510eabb5edc962016100
surajjayraman <suraj.jayraman@gmail.com>
2023-05-14 11:04:23 -0400

    Update with Array.map custom examples.

--- callback.js
+++ callback.js
@@ -35,3 +35,32 @@ pets.forEach(function(petName) {
 });
 
 pets.forEach(giveATreat);
+
+// Our Map implementation
+const ourMap = (arr, callback) => {
+  // create an output array
+  const output = [];
+  
+  // loop through the provided array
+  for (const element of arr) {
+    // call the provided callback with each element of the array AND store the return value
+    const returnVal = callback(element);
+  
+    // add the return value to the output array
+    output.push(returnVal);
+  }
+  
+  // return the output array
+  return output;
+};
+
+const names = ['alice', 'bob', 'carol'];
+
+const doOnEachLoop2 = name => `you are logged in as ${name}`;
+
+const ourMappedNames = ourMap(names, doOnEachLoop2);
+console.log(names);
+console.log(ourMappedNames);
+
+// use Array map in built fn
+console.log(names.map(name => doOnEachLoop2(name)));
