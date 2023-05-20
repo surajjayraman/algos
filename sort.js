18376f0688faa11ec25b6d86113deafabe28acd7
surajjayraman <suraj.jayraman@gmail.com>
2023-05-20 18:47:20 -0400

    Update code for advanced sort of complex objects.

--- sort.js
+++ sort.js
@@ -21,3 +21,28 @@ months.reverse();
 console.log(months);
 array1.sort(compareFnDesc);
 console.log(array1);
+
+// Advanced Sorting
+const students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 },
+  { id: 5, name: "zoidberg", age: 23 },
+];
+
+students.sort((a, b) => {
+  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+  
+  // where the names are equal sort by descending age
+  return (b.age - a.age);
+});
+
+console.log(students);
