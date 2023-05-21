8618c88e6b93a4a9a7008cb6e014b2d0253336df
surajjayraman <suraj.jayraman@gmail.com>
2023-05-21 09:16:07 -0400

    Update with iife func expression.

--- closure.js
+++ closure.js
@@ -9,3 +9,17 @@ const outer = function() {
   
 const foo = outer();
 foo();
+
+// Immediately-invoked function expression
+const iifoo = (function() {
+  const x = 10;
+  
+  const inner = function() {
+    console.log("Value of x is: " + x);
+  };
+  return inner;
+})(); // Notice the () at the end,
+// which will immediately invoke the function,
+// assigning the returned value (inner) to iifoo
+  
+iifoo();
