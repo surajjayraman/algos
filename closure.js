426909ba24acb5e545ea92b22d2fbcd5b81d3506
surajjayraman <suraj.jayraman@gmail.com>
2023-05-26 08:30:31 -0400

    Add code for countdownGenerator.

--- closure.js
+++ closure.js
@@ -88,10 +88,9 @@ console.log(rollDie());  // 1 (for example)
 // predict the next number while rolling a die
 const makeLoadedDie = function() {
   const list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
-  /* your code here */
+  /* closure code here */
   
   return function() {
-    /* your code here */
     return list[Math.floor(1 + Math.random() * 9)];
   };
 };
@@ -101,3 +100,23 @@ const rollLoadedDie = makeLoadedDie();
 console.log(rollLoadedDie());  // 5
 console.log(rollLoadedDie());  // 4
 console.log(rollLoadedDie());  // 6
+
+// Implement a function countdownGenerator()
+const countdownGenerator = function(x) {
+
+  return () => {
+    if (x === 0) console.log(`Blast Off!`);
+    if (x < 0) console.log(`Rockets already gone, bub!`);
+    if (x > 0) console.log(`T-minus ${x}`);
+    x--;
+  };
+    
+};
+  
+const countdown = countdownGenerator(3);
+countdown(); // T-minus 3...
+countdown(); // T-minus 2...
+countdown(); // T-minus 1...
+countdown(); // Blast Off!
+countdown(); // Rockets already gone, bub!
+countdown(); // Rockets already gone, bub!
