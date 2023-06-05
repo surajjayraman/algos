e1a7cc73156d1ee22bca0a554ebb307aa9556822
surajjayraman <suraj.jayraman@gmail.com>
2023-06-05 12:04:31 -0400

    Add code to build an object tree using recursion.

--- recursion.js
+++ recursion.js
@@ -16,3 +16,28 @@ const sumToOne = function(n) {
 };
   
 console.log(sumToOne(4));
+
+// build an object tree using recursion
+const categories = [
+  {id: 'animals', parent: null},
+  {id: 'mammals', parent: 'animals'},
+  {id: 'cats', parent: 'mammals'},
+  {id: 'dogs', parent: 'mammals'},
+  {id: 'chihuaha', parent: 'dogs'},
+  {id: 'labrador', parent: 'dogs'},
+  {id: 'persian', parent: 'cats'},
+  {id: 'siamese', parent: 'cats'},
+  {id: 'sweety', parent: 'chihuaha'}
+];
+
+const makeTree = (categories, parent) => {
+  const node = {};
+  categories
+    .filter(c => c.parent === parent)
+    .forEach(c => node[c.id] = makeTree(categories, c.id));
+  return node;
+};
+
+console.log(
+  JSON.stringify(makeTree(categories,null),null, 2)
+);
