3872426873cdce2a18170b8a8947a9f758e4f67a
surajjayraman <suraj.jayraman@gmail.com>
2023-06-11 10:57:49 -0400

    Add additional jest tests

--- animation.js
+++ animation.js
@@ -7,27 +7,27 @@ let moveSpeed = speed;
 let radius = 40;
 
 function moveBall() {
-    if (position + radius > 640) {
-        moveSpeed = -speed;
-    } else if (position - radius < 0) {
-        moveSpeed = speed;
-    }
-    position += moveSpeed;
+  if (position + radius > 640) {
+    moveSpeed = -speed;
+  } else if (position - radius < 0) {
+    moveSpeed = speed;
+  }
+  position += moveSpeed;
 }
 
 function drawBall() {
-    context.clearRect(0, 0, 640, 480);
+  context.clearRect(0, 0, 640, 480);
 
-    context.fillStyle = "#62687f";
-    context.beginPath();
-    context.arc(position, 50, radius, 0, 2 * Math.PI);
-    context.fill();
+  context.fillStyle = "#62687f";
+  context.beginPath();
+  context.arc(position, 50, radius, 0, 2 * Math.PI);
+  context.fill();
 }
 
 function animate() {
-    moveBall();
-    drawBall();
-    window.requestAnimationFrame(animate);
+  moveBall();
+  drawBall();
+  window.requestAnimationFrame(animate);
 }
 
 window.requestAnimationFrame(animate);
--- objComparison.test.js
+++ objComparison.test.js
@@ -7,3 +7,12 @@ test('returns object comparison true', () => {
 test('returns object comparison false', () => {
   expect(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })).toBe(false);
 });
+
+test('returns primitive comparison true', () => {
+  expect(eqObjects(11, 11)).toBe(true);
+});
+
+test('returns primitive comparison false', () => {
+  expect(eqObjects(11, 1)).toBe(false);
+});
+
